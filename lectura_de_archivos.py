2eb2deb7d8891d15f8f7de7c4b7a46aca80be402
Sebastian Quevedo <109982160+SebastianQuevedo@users.noreply.github.com>
2022-09-17 21:29:03 -0500

    Update lectura_de_archivos.py

--- lectura_de_archivos.py
+++ lectura_de_archivos.py
@@ -1,24 +1,42 @@
 import os
 import shutil
 import os.path
+#ingresan 
+Ubicacion_carpeta_origen="Downloads"
+ubicacion_carpeta_destino="Downloads"
+carpeta_origen="prueba_origen"
+carpeta_destino="prueba_destino"
 
-def lectura_de_archivos (info:[(str,str)])-> str:
-    if os.path.exists(info[a])==True:
-     for a in info:
-        conenido=os.listdir(a)
-        cantidad=len(contenido)
-     if os.path.exists(info[b])==True:
-       #movimiento_de_archivos(a,b)
-       print ("sss")
-     else:
-      nombre_carpeta=info[b]
-      os.mkdir(nombre_carpeta)
-      #movimiento_de_archivos   
-             
-    else:
-        print ("la carpeta de origen no existe")
-    retorno=""    
-    return retorno
+#definicion rutas
+ruta_base=os.getcwd()
+ruta_origen=os.path.join(ruta_base,Ubicacion_carpeta_origen,carpeta_origen)
 
-def movimiento_de_archivos (archivos[(str,int,str)]):
+ruta_destino=carpeta_destino
+archivos=os.listdir(ruta_origen)
+cantidad=len(archivos)
+
+   
+
+
+#comprobar carpeta y mover
+if os.path.exists(carpeta_origen)==True:
+  if os.path.exists(carpeta_destino)==True:
+   for g in archivos:
+    ruta_origen_completa=os.path.join(ruta_origen,archivos[g])
+    ruta_destino_completa=os.path.join(ruta_base,ubicacion_carpeta_destino,ruta_destino)
+    shutil.move(ruta_origen_completa, ruta_destino_completa)
+  else:
+   nombre_carpeta=carpeta_destino
+   os.mkdir(nombre_carpeta) 
+   for g in archivos:
+    ruta_origen_completa=os.path.join(ruta_origen,archivos[g])
+    ruta_destino_completa=os.path.join(ruta_base,ubicacion_carpeta_destino,ruta_destino)
+    shutil.move(ruta_origen_completa, ruta_destino_completa)
+else:
+ print ("la carpeta de origen no existe")
+
+
+
+
+     
     
